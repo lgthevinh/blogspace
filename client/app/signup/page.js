ac4f54f328a39b32327df42066c0b73b2d71c673
ltvinh <everwellmax@gmail.com>
2024-02-19 23:21:02 +0700

    update box for responsive

--- client/app/signup/page.js
+++ client/app/signup/page.js
@@ -9,10 +9,10 @@ export default function SignUpPage() {
   return (
     <div className="bg-blue-600">
       <div className="bg-blue-600 w-screen h-screen flex items-center content-center justify-center">
-        <div className="rounded-2xl bg-white border-slate-400 border shadow p-5">
+        <div className="rounded-2xl bg-white border-slate-400 border shadow my-3 p-5 max-w-96 min-w-36 w-full">
           <h1 className="text-blue-600 text-3xl font-semibold">blogspace</h1>
           <p className="text-slate-600 text-md mt-3">New to blogspace? Sign up now!</p>
-          <form className="form-control min-w-96">
+          <form className="form-control">
             <input type="text" className="input input-primary w-full mt-3 bg-white" placeholder="Username" />
             <input type="email" className="input input-primary w-full mt-3 bg-white" placeholder="Email" />
             <input type="password" className="input input-primary w-full mt-3 bg-white" placeholder="Password" />
