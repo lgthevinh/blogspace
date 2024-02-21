616fa779548478fa0bf0835dc6787554ed711679
ltvinh <everwellmax@gmail.com>
2024-02-21 16:02:26 +0700

    added handleLogin function

--- client/components/SideTab.js
+++ client/components/SideTab.js
@@ -1,6 +1,16 @@
+'use client'
+
+import { useState } from "react";
+
+import login_user from "@/app/services/loginService";
+
 const SideTab = (props) => {
   const is_logged_in = props.is_logged_in;
   const user_data = props.user;
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const social_media_list = user_data.social_media.map((object, index) => {
     return (
       <li key={index} className="">
@@ -10,6 +20,11 @@ const SideTab = (props) => {
       </li>
     );
   });
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    const data = await login_user(email, password);
+    console.log(data);
+  };
   return (
     <div className="h-fit sticky top-[4.5rem] lg:block hidden">
       {is_logged_in ? (
@@ -56,9 +71,9 @@ const SideTab = (props) => {
             Sign in to get personalized story recommendations, follow authors and topics you love, and interact with stories.
           </p>
           <form className="form-control">
-            <input type="text" className="input input-primary w-full mt-3 bg-white" placeholder="Username" />
-            <input type="password" className="input input-primary w-full mt-3 bg-white" placeholder="Password" />
-            <button className="btn btn-primary bg-blue-600 text-slate-50 hover:bg-blue-500 w-full mt-3">Sign in</button>
+            <input type="text" className="input input-primary w-full mt-3 bg-white text-slate-700" placeholder="Email" onChange={(e) => setEmail(e.target.value)}/>
+            <input type="password" className="input input-primary w-full mt-3 bg-white text-slate-700" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
+            <button className="btn btn-primary bg-blue-600 text-slate-50 hover:bg-blue-500 w-full mt-3" onClick={handleLogin}>Sign in</button>
           </form>
           <div className="divider before:bg-slate-400 after:bg-slate-400 text-slate-400 my-3 text-sm">Or</div>
           <p className="text-slate-500 text-sm font-semibold">
