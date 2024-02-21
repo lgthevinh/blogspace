282c89aa43e236c452d0c599439dd057f3b9c071
ltvinh <everwellmax@gmail.com>
2024-02-21 21:29:30 +0700

    Update loginService.js to log server URLs

--- client/app/services/loginService.js
+++ client/app/services/loginService.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const URL =  process.env.SERVER_URL || process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:8080/"
 
 const login_user = async (email, password) => {
-  console.log("URL: ", process.env);
+  console.log("URL: ", process.env.SERVER_URL, process.env.NEXT_PUBLIC_SERVER_URL);
   try {
     console.log("Email: ", email);
     console.log("Password: ", password);
